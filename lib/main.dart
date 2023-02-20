132c6994735ced35a871ad1f992b8ec6d67406e1
Youssef Dehbi <youssefdehbi59@gmail.com>
2023-02-20 14:21:34 +0100

    edit starting values of dices

--- lib/main.dart
+++ lib/main.dart
@@ -24,8 +24,8 @@ class DicePage extends StatefulWidget {
 }
 
 class _DicePageState extends State<DicePage> {
-  int leftDiceNumber = 1;
-  int rightDiceNumber = 1;
+  int leftDiceNumber = 2;
+  int rightDiceNumber = 2;
   @override
   Widget build(BuildContext context) {
     return Center(
